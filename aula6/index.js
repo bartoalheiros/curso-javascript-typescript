f2dd4b00487176ef652330c225f3dd641e1486a1
BartoAlheiros <alheirosb@gmail.com>
2024-04-02 16:47:28 -0300

    variaveis com let

---
+++ aula6/index.js
@@ -0,0 +1,7 @@
+let nomeCliente = 'João'; // Declarou a variável
+console.log(nomeCliente);
+
+// Não pode começar o nome de uma variável com um número
+let no1me;
+// Não podem conter espaços ou traços
+// let nome Cliente;
