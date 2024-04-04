d354d37646ceb7dba4446e3249a9d7ebf652b726
BartoAlheiros <alheirosb@gmail.com>
2024-04-04 12:46:16 -0300

    operadores aritmeticos

---
+++ aula11/index.js
@@ -0,0 +1,19 @@
+/** Operadores Aritméticos 
+ * + Adição / Concantenação
+ * ** Potenciação
+ * % Resto da divisão
+ */
+
+/** ()
+ **
+ * / %
+ + -
+ */
+
+/** Incremento = ++
+ Decremento = --
+ */
+
+let contador = 2;
+contador *= 2;
+console.log(contador);
