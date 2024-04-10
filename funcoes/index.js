830e7a2474a8fb00971544b4da60122235d3c38e
BartoAlheiros <alheirosb@gmail.com>
2024-04-10 16:31:34 -0300

    funcoes quase finalizado

---
+++ funcoes/index.js
@@ -0,0 +1,47 @@
+// function soma(x = 1, y = 1) {
+//   const resultado = x + y;
+//   return resultado;
+// }
+
+// const resultado = soma(4, 2);
+// console.log(resultado);
+
+// console.log(soma(2, 2));
+// console.log(resultado);
+
+// console.log(soma(2, 2));
+// console.log(soma(3, 1));
+// console.log(soma(5, 10));
+
+// function saudacao(nome) {
+//   return `Bom dia, ${nome}!`;
+// }
+
+// const variavel = saudacao('Luiz');
+// console.log(variavel);
+
+// função anônima
+// const raiz = function (n) {
+//   return n ** 0.5;
+// };
+
+// console.log(raiz(9));
+// console.log(raiz(16));
+// console.log(raiz(25));
+
+// arrow function
+// é uma forma mais moderna de escrever funções anônimas
+// const raiz = (n) => {
+//   return n ** 0.5;
+// };
+
+// console.log(raiz(9));
+// console.log(raiz(16));
+// console.log(raiz(25));
+
+// arrow function com um só argumento
+const raiz = n => n ** 0.5;
+
+console.log(raiz(9));
+console.log(raiz(16));
+console.log(raiz(25));
