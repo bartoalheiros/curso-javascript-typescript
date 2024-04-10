0e6d8f5578fcf1e971523ae07c1bfa48644ff69d
BartoAlheiros <alheirosb@gmail.com>
2024-04-10 11:42:52 -0300

    iniciando com arrays

---
+++ arrays/index.js
@@ -0,0 +1,23 @@
+//                0        1        2
+const alunos = ['Luiz', 'Maria', 'João'];
+
+
+
+console.log(alunos);
+
+// alunos.unshift('Luiza'); // Adiciona no começo
+// alunos.unshift('Fábio');
+
+// alunos.push('Luiza'); // Adiciona no fim
+// alunos.push('Fábio');
+
+// alunos[alunos.length] = 'Luiza'; // Adiciona no fim
+// alunos[alunos.length] = 'Fábio';
+// alunos[alunos.length] = 'Luana';
+
+// alunos[0] = 'Eduardo';
+// alunos[3] = 'Luiza';
+
+// console.log(alunos);
+// console.log(alunos[0]);
+// console.log(alunos[2]);
