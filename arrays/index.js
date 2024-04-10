0961ec1d22a1af04a40373a3a69d8e41e1c65eb7
BartoAlheiros <alheirosb@gmail.com>
2024-04-10 14:51:41 -0300

    aula arrays finalizada

--- arrays/index.js
+++ arrays/index.js
@@ -1,9 +1,22 @@
-//                0        1        2
-const alunos = ['Luiz', 'Maria', 'João'];
+//                0        1        2          3     4
+let alunos = ['Luiz', 'Maria', 'João']; // Luiza, Eduardo
+// alunos = 123;
 
+// console.log(typeof alunos);
+// console.log(alunos instanceof Array);
 
-
+delete alunos[1];
 console.log(alunos);
+console.log(alunos[1]);
+
+// alunos.push('Luiza'); // Adiciona no fim do array
+// alunos.push('Eduardo');
+
+// console.log(alunos.slice(0, -2));
+
+// const removido = alunos.shift();
+// console.log(`Removido: ${removido}`);
+// console.log(alunos);
 
 // alunos.unshift('Luiza'); // Adiciona no começo
 // alunos.unshift('Fábio');
