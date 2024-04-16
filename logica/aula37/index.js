c2c73eb30f182611d395432ed6a22defe6f178be
BartoAlheiros <alheirosb@gmail.com>
2024-04-16 15:00:21 -0300

    operadores de comparacao

---
+++ logica/aula37/index.js
@@ -0,0 +1,50 @@
+/*
+Operadores de comparação
+> maior que
+>= maior que ou igual a
+< menor que
+<= menor que ou igual a
+== igualdade (valor) ***************
+!= diferente (valor) ***************
+=== igualdade estrita (valor e tipo)
+!== diferente estrito (valor e tipo)
+*/
+
+// const num1 = 10;
+// const num2 = 10;
+// const comp = num1 !== num2;
+// console.log(comp);
+
+// Saída: false
+
+// const num1 = 10;
+// const num2 = '10';
+// const comp = num1 !== num2;
+// console.log(comp);
+
+// Saída: truew
+
+// comparador estrito: compara o valor e o tipo
+// const num1 = 10;
+// const num2 = 10;
+// const comp = num1 === num2;
+// console.log(comp);
+
+// Saída: true
+
+// const num1 = 10;
+// const num2 = '10';
+// const comp = num1 === num2;
+// console.log(comp);
+
+// Saída: false
+
+// em javascript o operador == ignora o tipo de dado, fazendo uma
+// conversao para number
+// const num1 = 10;
+// const num2 = '10';
+// const comp = num1 == num2;
+// console.log(comp);
+
+// const comp = 10 <= 11;
+// console.log(comp);
