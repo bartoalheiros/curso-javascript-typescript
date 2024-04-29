f1898c153851b61ffe4f424cdf02e4cb50ec5fea
BartoAlheiros <alheirosb@gmail.com>
2024-04-29 16:39:46 -0300

    atribuicao via desestruturacao iniciado

---
+++ logica/desestruturacao_arrays/index.js
@@ -0,0 +1,13 @@
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+
+// Atribuição via desestruturação
+
+// let a = 'A'; // B
+// let b = 'B'; // C
+// let c = 'C'; // A
+
+// const letras = [b, c, a];
+// [a, b, c] = letras;
+
+// console.log(a, b, c);
