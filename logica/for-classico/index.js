770cc695369d3dffe7af2c9b9288099478a3889e
BartoAlheiros <alheirosb@gmail.com>
2024-04-30 15:37:28 -0300

    logica for classico concluido

---
+++ logica/for-classico/index.js
@@ -0,0 +1,7 @@
+//                0       1       2
+const frutas = ['Maçã', 'Pêra', 'Uva', 'Luiz', 
+    'Otávio', 'João', 'Miranda'];
+
+for (let i = 0; i < frutas.length; i++) {
+  console.log(`Índice ${i}`, frutas[i]);
+}
