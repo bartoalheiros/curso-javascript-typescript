76fb7fa7638b2aabf19d2fd6e89206c96bfb072b
BartoAlheiros <alheirosb@gmail.com>
2024-05-03 10:07:49 -0300

    exercicio com for completo

--- logica/exercicios-for/assets/js/main.js
+++ logica/exercicios-for/assets/js/main.js
@@ -1,17 +1,24 @@
 const elementos = [
-  {tag: 'p', texto: 'Frase 1'}, // 0
+  {tag: 'p', texto: 'Qualquer texto que você quiser.'}, // 0
   {tag: 'div', texto: 'Frase 2'}, // 1
   {tag: 'footer', texto: 'Frase 3'},
   {tag: 'section', texto: 'Frase 4'},
 ];
 
 const container = document.querySelector('.container');
+const div = document.createElement('div');
 
 function renderiza (i) {
   const {tag, texto} = elementos[i];
-  return `<${tag}> ${texto} </${tag}>`;
+  let tagCriada = document.createElement(tag);
+  let textoCriado = document.createTextNode(texto);
+
+  tagCriada.appendChild(textoCriado);
+  div.appendChild(tagCriada);
 }
 
 for (i = 0; i < elementos.length; i++) {
-  container.innerHTML += renderiza(i);
+  renderiza(i);
 }
+
+container.appendChild(div);
